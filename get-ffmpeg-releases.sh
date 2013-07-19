102a294ad0d3c277fda7d273909093e8190ed448
faywong <philip584521@gmail.com>
2013-07-19 23:18:55 +0800

    Update for the FFmpeg-2.0 release

--- get-ffmpeg-releases.sh
+++ get-ffmpeg-releases.sh
@@ -4,6 +4,7 @@ mkdir releases
 cd releases
 
 wget --content-disposition "http://git.videolan.org/?p=ffmpeg.git;a=snapshot;h=HEAD;sf=tgz"
+wget http://ffmpeg.org/releases/ffmpeg-2.0.tar.bz2
 wget http://ffmpeg.org/releases/ffmpeg-1.2.tar.gz
 wget http://ffmpeg.org/releases/ffmpeg-1.1.4.tar.gz
 wget http://ffmpeg.org/releases/ffmpeg-1.0.6.tar.gz
--- makefiles/Android_.mk
+++ makefiles/Android_.mk
@@ -38,6 +38,9 @@ FFLIBS-yes :=
 include $(FFMPEG_ROOT_DIR)/$(FFMPEG_LIB_DIR)/Makefile.android
 -include $(FFMPEG_ROOT_DIR)/$(FFMPEG_LIB_DIR)/$(TARGET_ARCH)/Makefile
 
+ifeq ($(VERSION_BRANCH),2.0)
+    include $(FFMPEG_ROOT_DIR)/arch.mak
+endif
 ifeq ($(VERSION_BRANCH),1.2)
     include $(FFMPEG_ROOT_DIR)/arch.mak
 endif
